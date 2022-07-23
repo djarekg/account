24a042e88d1e304b0e781222971f9c13843ed034
Dustin Griffith <djarekg@gmail.com>
2022-07-23 16:42:34 -0400

    initial check-in

---
+++ Dg.Account.Budget.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Dg.Account.Budget.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Dg.Account.Budget/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Dg.Account.Budget.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    
+}
