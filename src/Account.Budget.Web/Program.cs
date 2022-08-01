96174b5aa46e7c4df1b44202e25ae10fd5242f22
Dustin Griffith <djarekg@gmail.com>
2022-08-01 18:52:27 -0400

    Create Project:  ASP.NET

--- src/Account.Budget.Web/Program.cs
+++ src/Account.Budget.Web/Program.cs
@@ -3,11 +3,11 @@ using Account.Budget.Web.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMemoryCache();
 
 builder.Services.AddDbContextScopedFactory();
 builder.Services.AddAccountDbContext(builder.Configuration);
