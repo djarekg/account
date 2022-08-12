27519896ccac35ba00190c25f8a31fb622ed0971
Dustin Griffith <djarekg@gmail.com>
2022-08-12 17:23:35 -0400

    footer

--- src/Account.Budget/projects/account-budget/src/app/layout/footer/footer.component.ts
+++ src/Account.Budget/projects/account-budget/src/app/layout/footer/footer.component.ts
@@ -1,9 +1,17 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
+function getDomSanitizer() {
+  return inject(DomSanitizer);
+}
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent {}
+export class FooterComponent {
+  licenseUrl = getDomSanitizer().bypassSecurityTrustResourceUrl(
+    'https://github.com/angular/components/blob/main/LICENSE',
+  );
+}
